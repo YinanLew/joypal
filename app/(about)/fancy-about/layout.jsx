5f81e6544eccf6a276b9efc57d37a03604e20241
Yinan Lu <yinan.l@steeresg.com>
2025-03-11 23:02:24 -0400

    feat: add languages switch funciton and content

--- app/(about)/fancy-about/layout.jsx
+++ app/(about)/fancy-about/layout.jsx
@@ -1,11 +1,9 @@
-import "./globals.css"; 
-
 export const metadata = {
-  title:
-    "Fancy About || Resonance — One & Multi Page React Nextjs Creative Template",
+  title: "About JoyPal Logistics || Your International Courier in Toronto",
   description:
-    "Resonance — One & Multi Page React Nextjs Creative Template",
+    "JoyPal Logistics is a professional courier service based in Toronto, providing hassle-free international and local delivery solutions. Discover how we make your deliveries seamless and reliable.",
 };
+
 
 export default function FancyAboutLayout({ children }) {
   return <>{children}</>;
